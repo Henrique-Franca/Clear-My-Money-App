59ac80722bbd1bc2abb72d12279d64ab65447d3d
Henrique França <94202846+Henrique-Franca@users.noreply.github.com>
2022-11-25 09:34:57 -0300

    Habilitando o CORS

---
+++ .history/backend/src/config/cors_20221125093208.js
@@ -0,0 +1,5 @@
+module.exports = (req, res, next) =>{
+    res.header('Access-Control-Allow-Origing', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X_Requested-With, Content-Type, Accept')
+}
