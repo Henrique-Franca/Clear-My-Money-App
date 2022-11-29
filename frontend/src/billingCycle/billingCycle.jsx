b3859dcf2066d650ec8ffec5096004e12bdaef1a
HenriqueFranca <henriqsf@gmail.com>
2022-11-29 14:32:37 -0300

    Componentes Estruturais das Abas

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
+
 
 class BillingCycle extends Component{
 
@@ -11,7 +15,14 @@ class BillingCycle extends Component{
 
                 <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
                 <Content>
-                    Clicols de Pacagemtos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
 
             </div>
