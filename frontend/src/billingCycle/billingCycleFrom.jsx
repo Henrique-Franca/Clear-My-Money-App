ba2d8801b7a9cda8698db4ad29046a1ba1ea125f
Henrique França <henriqsf@gmail.com>
2022-12-01 11:49:11 -0300

    BillingCycleForm com Redux-Form (Parte 01)

---
+++ frontend/src/billingCycle/billingCycleFrom.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class BillingCycleForm extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default BillingCycleForm
